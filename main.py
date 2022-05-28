cece5789ed82bbb78e7d8148a7b1352923a1a7db
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-28 14:06:05 -0300

    primeiro release

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import os
 import shutil
 import sys
 
+print("\n\n\n\n")
 try:
     pasta = sys.argv[1]
     # cria a pasta
     os.mkdir(pasta)
     os.mkdir(f'{pasta}//utils')
 
-    # 
     os.system(f"python -m venv {pasta}//venv")
-
     #
-    os.system(f"code .//{pasta}")
-
     source = './/.gitignore'
     destination = f'.//{pasta}//.gitignore'
     shutil.copyfile(source, destination)
@@ -22,7 +19,19 @@ try:
     destination = f'.//{pasta}//README.MD'
     shutil.copyfile(source, destination)
 
+    print("git init")
+    os.system(f"cd {pasta} & git init")
+    print("git add .")
+    os.system(f"cd {pasta} & git add .")
+    
+    print("git commit -m '☝ primeiro commit'")
+    os.system(f'cd {pasta} & git commit -m "☝ Primeiro commit"')
+    print('git -b "feat001"')
+    os.system(f'cd {pasta} & git -b "feat001"')
+    
+    os.system(f"code .//{pasta}")
 
+    print("\n\n\n\n")
 
 
 except Exception as e: 
